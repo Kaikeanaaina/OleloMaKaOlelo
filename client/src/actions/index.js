513cb937fd6d3291cc2578c65c8c144570219c4b
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-22 02:19:21 -1000

    added huaolelo acitons

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
-import { FETCH_USER} from './types'
+import { FETCH_USER, FETCH_HUAOLELO} from './types'
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user')
 
   dispatch({ type: FETCH_USER, payload: res.data })
+}
+
+export const fetchHuaolelo = () => async dispatch => {
+  const res = await axios.get('/api/huaolelo')
+
+  dispatch({ type: FETCH_HUAOLELO, payload: res.data})
 }
