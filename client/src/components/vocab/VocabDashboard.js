9825d877957f0624b6f159e4c2caffdc7199d074
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-22 02:15:57 -1000

    orginzing for vocab components

---
+++ client/src/components/vocab/VocabDashboard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
+
+
+export class VocabDashboard extends Component {
+    componentDidMount(){
+        this.props.fetchHuaolelo()
+    }
+    render() {
+        return (
+            <div>
+                <h1>vocab dashboard</h1>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({huaolelo}) {
+    return { huaolelo}
+}
+
+export default connect(mapStateToProps, actions)(VocabDashboard)
