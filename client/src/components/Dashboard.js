ff98aeeb6624e30ce4b1c99c9300b345aa17ecf0
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-22 02:18:44 -1000

    added vocab route

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Dashboard = () => {
   return (
     <div>
       <p>dashboard home, is logged in</p>
+      <Link to="/vocab">
+        <h2>vocab dashboard</h2>
+      </Link>
+      <h2>sentence structures</h2>
+      <h2>examples</h2>
     </div>
   )
 }
