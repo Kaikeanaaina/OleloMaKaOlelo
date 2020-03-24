890e242a961ae16da6a86a8b768c595a05a9e12a
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-24 12:23:01 -1000

    changed state to an empty array

--- client/src/reducers/wordGroupReducer.js
+++ client/src/reducers/wordGroupReducer.js
@@ -1,9 +1,9 @@
 import { FETCH_WORD_GROUPS } from '../actions/types.js'
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WORD_GROUPS:
-      return action.payload || false
+      return action.payload
     default:
       return state
   }
