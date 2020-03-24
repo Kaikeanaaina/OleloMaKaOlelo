b26542959ba4c9d733ea3fae462aaa6aae179a0e
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-23 21:38:01 -1000

    Post Groupwork now validates if one exists already

--- routes/huaoleloRoutes.js
+++ routes/huaoleloRoutes.js
@@ -5,10 +5,59 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 
 const Huaolelo = mongoose.model('huaolelo')
+const WordGroup = mongoose.model('wordgroups')
 
 module.exports = app => {
     app.get('/api/huaolelo', requireLogin, async (req, res) => {
         console.log('reached the back')
         return
+    })
+
+    app.get('/api/wordGroups', async (req, res) => {
+        console.log('getting wordgroup')
+        const wordgroups = await WordGroup.find({}).select({})
+
+        res.send(wordgroups)
+    })
+
+    app.post('/api/wordGroups', async (req, res) => {
+
+        if (!req.body.title) {
+            return res.send('there is no body in the request')
+        }
+
+        const { title } = req.body
+        const existingTitle = await WordGroup.find({title})
+
+        if (existingTitle.length) {
+            return res.send('title already exists in the database')
+        } 
+
+        const wordGroup = new WordGroup({
+            title
+        })
+
+        try {
+            await wordGroup.save()
+
+            res.send(wordGroup)
+          } catch (err) {
+            res.status(422).send(err)
+          }
+    })
+
+    app.delete('/api/wordGroups', async (req, res) => {
+        return console.log('delete wordgroup')
+        //const { title } = req.body
+
+        // const wordGroups = WordGroup.deleteMany({ title }, function (err) {});
+        // console.log('these are the deleted wordgroups', wordGroups)
+        // try {
+        //     await wordGroups.save()
+      
+        //     res.send(wordGroups)
+        //   } catch (err) {
+        //     res.status(422).send(err)
+        //   }
     })
 }
