6726807e1dd02699249c1308b9a462cef17fa061
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-24 11:47:54 -1000

    wordGroups in the index reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import huaoleloReducer from './huaoleloReducer'
+import wordGroupReducer from './wordGroupReducer'
 
 export default combineReducers({
   auth: authReducer,
-  huaolelo: huaoleloReducer
+  huaolelo: huaoleloReducer,
+  wordGroups: wordGroupReducer
 })
