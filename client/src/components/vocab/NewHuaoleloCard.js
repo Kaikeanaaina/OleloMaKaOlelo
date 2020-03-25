cbea223c1cb1b335b01cc39cc2194fa8214dd6df
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-24 16:58:22 -1000

    created new huaolelo card

---
+++ client/src/components/vocab/NewHuaoleloCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const NewHuaoleloCard = () => {
+    return (
+        <div className="card darken-1">
+            <div className="card-content" >
+                <h2>New Huaolelo Card</h2>
+            </div>
+        </div>
+    )
+}
+
+export default NewHuaoleloCard
