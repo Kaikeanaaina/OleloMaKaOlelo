00981cde74b422dd006c3e679ec58bade1d2f583
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-29 17:06:31 -1000

    select tag console logs event handler

--- client/src/components/vocab/NewHuaoleloCard.js
+++ client/src/components/vocab/NewHuaoleloCard.js
@@ -1,14 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import 'materialize-css';
+import { Select } from 'react-materialize';
+import { connect } from 'react-redux'
 
 
-const NewHuaoleloCard = () => {
-    return (
-        <div className="card darken-1">
-            <div className="card-content" >
-                <h2>New Huaolelo Card</h2>
+class NewHuaoleloCard extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSelectWordGroup = this.handleSelectWordGroup.bind(this)
+        this.state = {
+            word: ''
+        }
+    }
+    handleSelectWordGroup(event){
+        event.preventDefault()
+        console.log(event.target.value)
+    }
+    renderWordList(){
+        return this.props.wordGroups.sort().map(wordGroup => {
+            return (
+                <option key={wordGroup._id} value={wordGroup.title}>
+                    {wordGroup.title}
+                </option>
+            )
+        })
+    }
+    render(){
+        return (
+            <div className="card darken-1">
+                <div className="card-content" >
+                    <h2>New Huaolelo Card</h2>
+                    <Select
+                        id="Select-9"
+                        multiple={false}
+                        onChange={this.handleSelectWordGroup}
+                        options={{
+                            classes: '',
+                            dropdownOptions: {
+                                alignment: 'left',
+                                autoTrigger: true,
+                                closeOnClick: true,
+                                constrainWidth: true,
+                                container: null,
+                                coverTrigger: true,
+                                hover: false,
+                                inDuration: 150,
+                                onCloseEnd: null,
+                                onCloseStart: null,
+                                onOpenEnd: null,
+                                onOpenStart: null,
+                                outDuration: 250
+                            }
+                        }}
+                        value=""
+                        >
+                        <option
+                            disabled
+                            value=""
+                            >
+                            Choose your option
+                        </option>
+                        
+                        {this.renderWordList()}
+                    </Select>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default NewHuaoleloCard
+function mapStateToProps({ wordGroups }) {
+    return { wordGroups}
+}
+
+export default connect(mapStateToProps)(NewHuaoleloCard)
