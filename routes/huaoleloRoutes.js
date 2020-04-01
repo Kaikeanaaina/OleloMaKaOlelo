3352c2fa4ec8e3e5733f7f8e5fa4071311a08a5b
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-03-31 19:33:26 -1000

    temp console.log

--- routes/huaoleloRoutes.js
+++ routes/huaoleloRoutes.js
@@ -13,36 +13,42 @@ module.exports = app => {
         return
     })
 
+    app.post('/api/huaolelo', requireLogin, async (req, res) => {
+        console.log('this is the huaolelo posting', req.body)
+        return
+    })
+
     app.get('/api/wordGroups', async (req, res) => {
         const wordgroups = await WordGroup.find({}).select({})
 
         res.send(wordgroups)
     })
 
-    app.post('/api/wordGroups', async (req, res) => {
+    app.post('/api/wordGroup', async (req, res) => {
+        console.log('this is the word group posting, ', res.body )
+        return
+        // if (!req.body.title) {
+        //     return res.send('there is no body in the request')
+        // }
 
-        if (!req.body.title) {
-            return res.send('there is no body in the request')
-        }
+        // const { title } = req.body
+        // const existingTitle = await WordGroup.find({title})
 
-        const { title } = req.body
-        const existingTitle = await WordGroup.find({title})
+        // if (existingTitle.length) {
+        //     return res.send('title already exists in the database')
+        // } 
 
-        if (existingTitle.length) {
-            return res.send('title already exists in the database')
-        } 
+        // const wordGroup = new WordGroup({
+        //     title
+        // })
 
-        const wordGroup = new WordGroup({
-            title
-        })
+        // try {
+        //     await wordGroup.save()
 
-        try {
-            await wordGroup.save()
-
-            res.send(wordGroup)
-          } catch (err) {
-            res.status(422).send(err)
-          }
+        //     res.send(wordGroup)
+        //   } catch (err) {
+        //     res.status(422).send(err)
+        //   }
     })
 
     app.delete('/api/wordGroups', async (req, res) => {
