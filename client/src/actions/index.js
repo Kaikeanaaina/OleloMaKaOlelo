c72920104e7fe853c6c06a7ff0bc5939423c9cdf
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-04-07 00:56:16 -1000

    created edit and delete word group for the index.js in actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,31 +3,36 @@ import { FETCH_USER, FETCH_HUAOLELO, FETCH_WORD_GROUPS} from './types'
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user')
-
   dispatch({ type: FETCH_USER, payload: res.data })
 }
 
 export const fetchHuaolelo = () => async dispatch => {
   const res = await axios.get('/api/huaolelo')
-
   dispatch({ type: FETCH_HUAOLELO, payload: res.data})
 }
 
 export const submitHuaolelo = (values, history) => async dispatch => {
   const res = await axios.post('/api/huaolelo', values)
-
   //history.push('/huaolelo')
   dispatch({ type: FETCH_USER, payload: res.data})
 }
 
 export const fetchWordGroups = () => async dispatch => {
   const res = await axios.get('/api/wordGroups')
-
   dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
 }
 
 export const submitWordGroup = (values, history) => async dispatch => {
   const res = await axios.post('/api/wordGroup', values)
-  
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+}
+
+export const editWordGroup = (values, history) => async dispatch => {
+  const res = await axios.put('/api/wordGroup', values)
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+}
+
+export const deleteWordGroup = (values, history) => async dispatch => {
+  const res = await axios.delete('/api/wordGroup', values)
   dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
 }
