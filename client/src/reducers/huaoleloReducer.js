f0d40c612616905d62ff123cd35abb28a99ea465
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-04-12 12:02:39 -1000

    removing false

--- client/src/reducers/huaoleloReducer.js
+++ client/src/reducers/huaoleloReducer.js
@@ -3,7 +3,7 @@ import { FETCH_HUAOLELO } from '../actions/types.js'
 export default function(state = [], action) {
     switch (action.type) {
         case FETCH_HUAOLELO:
-            return action.payload || false
+            return action.payload
         default:
             return state
     }
