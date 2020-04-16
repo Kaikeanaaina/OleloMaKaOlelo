c0d01cf41cfd142acfa7b7e38ec569a1225b753c
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-04-16 09:16:02 -1000

    renders wordgroups for hua olelo

--- client/src/components/vocab/HuaoleloCard.js
+++ client/src/components/vocab/HuaoleloCard.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 class HuaoleloCard extends Component {
+    renderWordGroups() {
+        return this.props.wordGroups.sort(function(firstWordGroup,secondWordGroup){
+            return firstWordGroup.localeCompare(secondWordGroup)
+        }).map(wordgroup => {
+            return (
+                <div key={wordgroup}>
+                    <p>{wordgroup}</p>
+                </div>
+            )
+        })
+    }
     render() {
-        const {huaolelo, unuhi, wordGroups} = this.props
+        const { huaolelo, unuhi } = this.props
         return (
             <div>
                 <h5>{huaolelo}</h5>
                 <p>{unuhi}</p>
-                <p>{wordGroups}</p>
+                {this.renderWordGroups()}
             </div>
         )
     }
