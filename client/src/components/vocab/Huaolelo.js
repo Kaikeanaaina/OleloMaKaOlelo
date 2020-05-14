b9a656a57a3c14142585ea3716fb6e69b9e7eb78
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-05-14 00:10:19 -1000

    added huaolelo page

---
+++ client/src/components/vocab/Huaolelo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Huaolelo extends Component {
+    render() {
+        return (
+            <div>
+                <h5>Huaolelo</h5>
+            </div>
+        )
+    }
+}
+
+export default Huaolelo
