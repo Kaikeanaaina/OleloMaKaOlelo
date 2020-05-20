7d022a8ca11aeb545d234d369a7dc9c115953dda
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-05-20 11:21:06 -1000

    removed fetchHighlightHuaolelo

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@
 // *Action* is the type and payload
 
 import axios from 'axios'
-import { FETCH_USER, FETCH_ALL_HUAOLELO, FETCH_WORD_GROUPS, HIGHLIGHT_A_HUAOLELO, FETCH_HIGHLIGHTED_HUAOLELO} from './types'
+import { FETCH_USER, FETCH_ALL_HUAOLELO, FETCH_WORD_GROUPS, HIGHLIGHT_A_HUAOLELO } from './types'
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user')
@@ -11,50 +11,51 @@ export const fetchUser = () => async dispatch => {
 
 export const fetchNaHuaolelo = () => async dispatch => {
   const res = await axios.get('/api/huaolelo')
-  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data})
+  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data })
 }
 
 export const submitHuaolelo = (values, history) => async dispatch => {
   const res = await axios.post('/api/huaolelo', values)
   //history.push('/huaolelo')
-  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data})
+  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data })
 }
 
 export const editHuaolelo = (values, history) => async dispatch => {
   const res = await axios.put('/api/huaolelo', values)
   //history.push('/huaolelo')
-  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data})
+  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data })
 }
 
 export const deleteHuaolelo = (values, history) => async dispatch => {
   const res = await axios.delete(`/api/huaolelo/$(values._id)`, values)
-  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data})
+  dispatch({ type: FETCH_ALL_HUAOLELO, payload: res.data })
 }
 
 export const highlightAHuaolelo = (values, history) => async dispatch => {
-  dispatch({ type: HIGHLIGHT_A_HUAOLELO, payload: values})
+  dispatch({ type: HIGHLIGHT_A_HUAOLELO, payload: values })
 }
 
-export const fetchHighlightedHuaolelo  = () => async dispatch => {
-  dispatch({type: FETCH_HIGHLIGHTED_HUAOLELO})
+export const fetchHighlightedHuaolelo = (paramsId) => async dispatch => {
+  const res = await axios.get(`/api/huaolelo/${paramsId}`)
+  dispatch({ type: HIGHLIGHT_A_HUAOLELO, payload: res.data })
 }
 
 export const fetchWordGroups = () => async dispatch => {
   const res = await axios.get('/api/wordGroups')
-  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data })
 }
 
 export const submitWordGroup = (values, history) => async dispatch => {
   const res = await axios.post('/api/wordGroup', values)
-  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data })
 }
 
 export const editWordGroup = (values, history) => async dispatch => {
   const res = await axios.put('/api/wordGroup', values)
-  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data })
 }
 
 export const deleteWordGroup = (values, history) => async dispatch => {
   const res = await axios.delete(`/api/wordGroup/${values._id}`, values)
-  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data})
+  dispatch({ type: FETCH_WORD_GROUPS, payload: res.data })
 }
